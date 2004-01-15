1b8b09929d17c4e1a905f26a1d3adbc0d52e5b3c
Sonia Dimitrov <sdimitro>
2004-01-15 15:14:08 +0000

    *** empty log message ***

--- sdk.tests/testConfigs/linux-gtk/testAll.sh
+++ sdk.tests/testConfigs/linux-gtk/testAll.sh
@@ -1,5 +1,5 @@
 # !/bin/sh
-
+cd .
 #environment variables
 PATH=.:/bin:/usr/bin:/usr/bin/X11:/usr/local/bin:/usr/X11R6/bin:`pwd`/../linux;export PATH
 xhost +$HOSTNAME
@@ -13,6 +13,6 @@ export LD_ASSUME_KERNEL LD_LIBRARY_PATH USERNAME DISPLAY
 
 #execute command to run tests
 
-runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2/jre/bin/java> linux.gtk_consolelog.txt
+./runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2/jre/bin/java> linux.gtk_consolelog.txt
 
 
