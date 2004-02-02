8b4eb9129d6705010c9b4e72c00d47762c7d6977
Kim Moir <kmoir>
2004-02-02 20:11:21 +0000

    update jdks to 1.4.2_03

--- sdk.tests/testConfigs/linux-gtk/testAll.sh
+++ sdk.tests/testConfigs/linux-gtk/testAll.sh
@@ -13,6 +13,6 @@ export LD_ASSUME_KERNEL LD_LIBRARY_PATH USERNAME DISPLAY
 
 #execute command to run tests
 
-./runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2/jre/bin/java> linux.gtk_consolelog.txt
+./runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2_03/jre/bin/java> linux.gtk_consolelog.txt
 
 
