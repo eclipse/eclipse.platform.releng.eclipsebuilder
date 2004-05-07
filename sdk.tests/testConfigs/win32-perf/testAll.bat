206699aff578bf59520ab9b856269918c6823f5f
Sonia Dimitrov <sdimitro>
2004-05-07 18:33:22 +0000

    remove unnecessary test config, add windows performance test config

---
+++ sdk.tests/testConfigs/win32-perf/testAll.bat
@@ -0,0 +1,9 @@
+@echo off
+cd %1
+REM test script
+
+REM add the extra binaries to the system path
+set PATH=%PATH%;%1\..\windowsBin
+
+REM run all tests
+call runtests.bat -vm ..\jdk1.4.2_03\jre\bin\java "-Dplatform=winxp" "-Dperf.host=eclipseperf.torolab.ibm.com" "-Dperf.port=9080" "-Dperf.id=sdimitro" >> %2
