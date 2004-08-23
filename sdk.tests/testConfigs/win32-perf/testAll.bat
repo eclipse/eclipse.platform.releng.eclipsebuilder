bb6df4c0f102925c9fabfa45460c362b6ea777d6
Sonia Dimitrov <sdimitro>
2004-08-23 19:33:53 +0000

    changes for startup test

--- sdk.tests/testConfigs/win32-perf/testAll.bat
+++ sdk.tests/testConfigs/win32-perf/testAll.bat
@@ -2,8 +2,10 @@
 cd %1
 REM test script
 
+set APT_HOME=%1\apt
+
 REM add the extra binaries to the system path
-set PATH=%PATH%;%1\..\windowsBin
+set PATH=%PATH%;%1\..\windowsBin;%1\..\jre\bin
 
 REM run all tests
-call runtests.bat -vm ..\jre\bin\java -properties vm.properties "-Dperformance=true" "-Dplatform=winxp_perf" "-Dperf.host=eclipseperf.torolab.ibm.com" "-Dperf.port=9080" "-Dperf.id=sdimitro" >> %2
+call runtests.bat -properties vm.properties "-Dperformance=true" "-Dplatform=winxp_perf" "-Dperf.host=eclipseperf.torolab.ibm.com" "-Dperf.port=9080" "-Dperf.id=sdimitro" >> %2
