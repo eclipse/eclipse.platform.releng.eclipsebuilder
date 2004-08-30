69382ada39fd4f02a11f7377934959df8e3599dc
Sonia Dimitrov <sdimitro>
2004-08-30 21:01:55 +0000

    *** empty log message ***

--- sdk.tests/testConfigs/win32-perf/testAll.bat
+++ sdk.tests/testConfigs/win32-perf/testAll.bat
@@ -5,5 +5,9 @@ REM test script
 REM add the extra binaries to the system path
 set PATH=%PATH%;%1\..\windowsBin
 
+mkdir results\xml
+mkdir results\html
+mkdir results\performance
+
 REM run all tests
 call runtests.bat -vm ..\jre\bin\java -properties vm.properties "-Dtest.target=performance" "-Dperformance=true" "-Dplatform=winxp_perf" "-Dperf.host=eclipseperf.torolab.ibm.com" "-Dperf.port=9080" "-Dperf.id=sdimitro" >> %2
