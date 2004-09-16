553d81a47ed1efa6524131d4b35aea76a029aa57
Sonia Dimitrov <sdimitro>
2004-09-16 18:35:57 +0000

    changes to install Cloudscape

--- sdk.tests/testConfigs/win32-perf/testAll.bat
+++ sdk.tests/testConfigs/win32-perf/testAll.bat
@@ -9,5 +9,8 @@ mkdir results\xml
 mkdir results\html
 mkdir results\performance
 
+REM add Cloudscape plugin to junit tests zip file
+zip eclipse-junit-tests-%3%.zip -rm eclipse
+
 REM run all tests
 call runtests.bat -vm ..\jdk1.4.2_05\jre\bin\java -properties vm.properties "-Dtest.target=performance">> %2
