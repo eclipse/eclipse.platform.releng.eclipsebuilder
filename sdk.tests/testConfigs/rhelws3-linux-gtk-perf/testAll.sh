db412fbd2fbc416f7f318bca33f3fe12db0b0078
Sonia Dimitrov <sdimitro>
2004-10-25 20:40:38 +0000

    setup for performance testing on linux gtk

---
+++ sdk.tests/testConfigs/rhelws3-linux-gtk-perf/testAll.sh
@@ -0,0 +1,18 @@
+# !/bin/sh
+cd .
+#environment variables
+PATH=.:/bin:/usr/bin:/usr/bin/X11:/usr/local/bin:/usr/X11R6/bin:`pwd`/../linux;export PATH
+xhost +$HOSTNAME
+MOZILLA_FIVE_HOME=/usr/lib/mozilla-1.4;export MOZILLA_FIVE_HOME
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MOZILLA_FIVE_HOME
+USERNAME=`whoami`
+DISPLAY=$HOSTNAME:0.0
+ulimit -c unlimited
+
+export LD_LIBRARY_PATH USERNAME DISPLAY
+
+#execute command to run tests
+
+./runtests -os linux -ws gtk -arch x86 -vm ../jdk1.4.2_03/jre/bin/java -properties vm.properties -Dtest.target=performance -Dplatform=linux.gtk.perf> linux.gtk.perf_consolelog.txt
+
+
