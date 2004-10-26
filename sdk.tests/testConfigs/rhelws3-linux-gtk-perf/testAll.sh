80e6680540e6fd0ffd24ea5ea0d31821260475d2
Sonia Dimitrov <sdimitro>
2004-10-26 15:09:57 +0000

    setup for performance testing on linux gtk

--- sdk.tests/testConfigs/rhelws3-linux-gtk-perf/testAll.sh
+++ sdk.tests/testConfigs/rhelws3-linux-gtk-perf/testAll.sh
@@ -13,6 +13,6 @@ export LD_LIBRARY_PATH USERNAME DISPLAY
 
 #execute command to run tests
 
-./runtests -os linux -ws gtk -arch x86 -vm ../jdk1.4.2_03/jre/bin/java -properties vm.properties -Dtest.target=performance -Dplatform=linux.gtk.perf> linux.gtk.perf_consolelog.txt
+./runtests -os linux -ws gtk -arch x86 -vm ../jdk1.4.2_06/jre/bin/java -properties vm.properties -Dtest.target=performance -Dplatform=linux.gtk.perf> linux.gtk.perf_consolelog.txt
 
 
