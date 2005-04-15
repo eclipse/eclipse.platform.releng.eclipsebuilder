61f8140fb308d5472bf7511f76daa5bcd1ba581c
Sonia Dimitrov <sdimitro>
2005-04-15 18:11:18 +0000

    changes for move to 1.4.2 08 JDK level

--- sdk.tests/testConfigs/rhelws3-linux-gtk/testAll.sh
+++ sdk.tests/testConfigs/rhelws3-linux-gtk/testAll.sh
@@ -13,6 +13,6 @@ export LD_LIBRARY_PATH USERNAME DISPLAY
 
 #execute command to run tests
 
-./runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2_06/jre/bin/java> linux.gtk_consolelog.txt
+./runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2_08/jre/bin/java> linux.gtk_consolelog.txt
 
 
