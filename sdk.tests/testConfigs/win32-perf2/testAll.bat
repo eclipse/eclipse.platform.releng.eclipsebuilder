23dcde5c7f2c15462ccc97e07e95142ba3327c5e
Sonia Dimitrov <sdimitro>
2005-05-01 12:50:40 +0000

    fix typo

--- sdk.tests/testConfigs/win32-perf2/testAll.bat
+++ sdk.tests/testConfigs/win32-perf2/testAll.bat
@@ -13,4 +13,4 @@ REM add Cloudscape plugin to junit tests zip file
 zip eclipse-junit-tests-%3%.zip -rm eclipse
 
 REM run all tests
-call runtests.bat -vm ..\jdk1.4.2_08\jre\bin\java -properties vm.properties "-Dtest.target=performance" "-Dplatform=win32.perf2" > %2
+call runtests.bat -vm ..\jdk1.4.2_08\jre\bin\java -properties vm.properties "-Dtest.target=performance" "-Dplatform=win32perf2" > %2
