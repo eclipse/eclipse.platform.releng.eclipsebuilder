a6e9bcf45a72ed5c73c12b2c525f14b8b5f31646
Kim Moir <kmoir>
2005-05-04 17:09:46 +0000

    synching with releng_test branch

--- sdk.tests/testConfigs/rhelws3-linux-gtk/testAll.sh
+++ sdk.tests/testConfigs/rhelws3-linux-gtk/testAll.sh
@@ -3,7 +3,7 @@ cd .
 #environment variables
 PATH=.:/bin:/usr/bin:/usr/bin/X11:/usr/local/bin:/usr/X11R6/bin:`pwd`/../linux;export PATH
 xhost +$HOSTNAME
-MOZILLA_FIVE_HOME=/usr/lib/mozilla-1.4.4;export MOZILLA_FIVE_HOME 
+MOZILLA_FIVE_HOME=/usr/lib/mozilla-1.7.7;export MOZILLA_FIVE_HOME 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MOZILLA_FIVE_HOME 
 USERNAME=`whoami`
 DISPLAY=$HOSTNAME:0.0
@@ -13,6 +13,6 @@ export LD_LIBRARY_PATH USERNAME DISPLAY
 
 #execute command to run tests
 
-./runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2_06/jre/bin/java> linux.gtk_consolelog.txt
+./runtests -os linux -ws gtk -arch x86 -Dplatform=linux.gtk -vm ../jdk1.4.2_08/jre/bin/java> linux.gtk_consolelog.txt
 
 
