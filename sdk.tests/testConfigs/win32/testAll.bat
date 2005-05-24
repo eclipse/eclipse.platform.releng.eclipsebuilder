3dda7513f17cdce40d17cd3a23da1c2a4d73099d
Sonia Dimitrov <sdimitro>
2005-05-24 14:33:38 +0000

    changes to run JDT Core tests on 1.5 vm

--- sdk.tests/testConfigs/win32/testAll.bat
+++ sdk.tests/testConfigs/win32/testAll.bat
@@ -7,4 +7,7 @@ set PATH=%PATH%;%1\..\windowsBin
 
 REM run all tests
 call runtests.bat -vm ..\jdk1.4.2_08\jre\bin\java "-Dplatform=winxp">> %2
+
+REM run JDT Core tests on 1.5 vm
+call runtests.bat -vm ..\jdk1.5.0_03\jre\bin\java all5.0 "-Dplatform=winxp">> %2
 exit
