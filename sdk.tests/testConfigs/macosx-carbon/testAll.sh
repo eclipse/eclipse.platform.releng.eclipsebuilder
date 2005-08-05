da5764a30736a84daa1d1febfbc1b62bef74ac39
Sonia Dimitrov <sdimitro>
2005-08-05 17:04:30 +0000

    add testing for Mac

---
+++ sdk.tests/testConfigs/macosx-carbon/testAll.sh
@@ -0,0 +1,8 @@
+# !/bin/sh
+ulimit -c unlimited
+
+#execute command to run tests
+./runtests -os macosx -ws carbon -arch ppc -properties vm.properties> macosx.carbon_consolelog.txt
+
+#run JDT Core tests on 1.5 vm
+./runtests -os macosx -ws carbon -arch ppc -vm ../jdk1.5.0_03/jre/bin/java -properties vm.properties all5.0>> macosx.carbon_consolelog.txt
