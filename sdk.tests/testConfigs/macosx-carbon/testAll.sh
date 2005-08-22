e87962494f59fdf4cf74de24a98c29d6244c138c
Sonia Dimitrov <sdimitro>
2005-08-22 18:11:06 +0000

    add vm arg for running on Mac

--- sdk.tests/testConfigs/macosx-carbon/testAll.sh
+++ sdk.tests/testConfigs/macosx-carbon/testAll.sh
@@ -2,7 +2,7 @@
 ulimit -c unlimited
 
 #execute command to run tests
-./runtests -os macosx -ws carbon -arch ppc -properties vm.properties> macosx.carbon_consolelog.txt
+./runtests -vm "java -XstartOnFirstThread" -os macosx -ws carbon -arch ppc -properties vm.properties> macosx.carbon_consolelog.txt
 
 #run JDT Core tests on 1.5 vm
-#./runtests -os macosx -ws carbon -arch ppc -vm ../jdk1.5.0_03/jre/bin/java -properties vm.properties all5.0>> macosx.carbon_consolelog.txt
+#./runtests -os macosx -ws carbon -arch ppc -vm "../jdk1.5.0_03/jre/bin/java -XstartOnFirstThread" -properties vm.properties all5.0>> macosx.carbon_consolelog.txt
