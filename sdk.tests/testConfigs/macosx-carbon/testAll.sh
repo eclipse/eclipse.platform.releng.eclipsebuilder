a16608670040d3de2cac1d668488985fa7231598
Sonia Dimitrov <sdimitro>
2005-09-13 20:17:28 +0000

    add JDT Core testing on Mac OS X

--- sdk.tests/testConfigs/macosx-carbon/testAll.sh
+++ sdk.tests/testConfigs/macosx-carbon/testAll.sh
@@ -5,4 +5,4 @@ ulimit -c unlimited
 ./runtests -vm "java -XstartOnFirstThread" -os macosx -ws carbon -arch ppc -properties vm.properties> macosx.carbon_consolelog.txt
 
 #run JDT Core tests on 1.5 vm
-#./runtests -os macosx -ws carbon -arch ppc -vm "/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Commands/java -XstartOnFirstThread" -properties vm.properties all5.0>> macosx.carbon_consolelog.txt
+./runtests -os macosx -ws carbon -arch ppc -vm "/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Commands/java -XstartOnFirstThread" -properties vm.properties all5.0>> macosx.carbon_consolelog.txt
