36bd52bf06edc9f46ce7f5e96cc364c325ea3cbf
Kim Moir <kmoir>
2005-12-05 18:59:13 +0000

    changes to support testing on rh es ws 4 as per 3.2 project plan

---
+++ sdk.tests/testConfigs/rhelws4-linux-gtk/testAll.sh
@@ -0,0 +1,17 @@
+# !/bin/sh
+cd .
+#environment variables
+PATH=.:/bin:/usr/bin:/usr/bin/X11:/usr/local/bin:/usr/X11R6/bin:`pwd`/../linux;export PATH
+xhost +$HOSTNAME
+MOZILLA_FIVE_HOME=/usr/lib/mozilla-1.7.12;export MOZILLA_FIVE_HOME 
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MOZILLA_FIVE_HOME 
+USERNAME=`whoami`
+DISPLAY=$HOSTNAME:0.0
+ulimit -c unlimited
+
+export LD_LIBRARY_PATH USERNAME DISPLAY
+
+#execute command to run tests
+
+./runtests -os linux -ws gtk -arch x86 -vm ../jdk1.4.2_08/jre/bin/java -properties vm.properties> linuxws4.gtk_consolelog.txt
+
