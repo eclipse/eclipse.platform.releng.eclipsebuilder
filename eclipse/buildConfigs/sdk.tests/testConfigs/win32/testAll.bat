78e561af27a3fc4438dbd25bac01d01f0a3c6a3c
Sonia Dimitrov <sdimitro>
2006-03-20 14:59:18 +0000

    switch to testing with the 1.4.2_10 vm

--- eclipse/buildConfigs/sdk.tests/testConfigs/win32/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win32/testAll.bat
@@ -6,6 +6,6 @@ REM add the extra binaries to the system path
 set PATH=%PATH%;%1\..\windowsBin
 
 REM run all tests
-call runtests.bat -vm ..\jdk1.4.2_08\jre\bin\java -properties vm.properties > %2
+call runtests.bat -vm ..\jdk1.4.2_10\jre\bin\java -properties vm.properties > %2
 
 exit
