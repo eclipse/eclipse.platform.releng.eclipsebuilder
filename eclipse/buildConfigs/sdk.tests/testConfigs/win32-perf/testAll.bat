fd940e3169323670c972d9eae63827d3f513e297
Sonia Dimitrov <sdimitro>
2006-03-23 19:43:58 +0000

    merge with releng_test branch

---
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win32-perf/testAll.bat
@@ -0,0 +1,13 @@
+@echo off
+cd %1
+REM test script
+
+mkdir results\xml
+mkdir results\html
+
+REM add Cloudscape plugin to junit tests zip file
+zip eclipse-junit-tests-%3%.zip -rm eclipse
+
+REM run all tests
+call runtests.bat -vm ..\jdk1.4.2_08\jre\bin\java -properties vm.properties "-Dtest.target=performance" "-Dplatform=win32perf" > %2
+exit
