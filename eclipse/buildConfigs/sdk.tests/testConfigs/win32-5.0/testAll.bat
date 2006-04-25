0b42b2e79f6b739f116dbd3d563ee7753d83ca2f
Sonia Dimitrov <sdimitro>
2006-04-25 15:20:15 +0000

    *** empty log message ***

--- eclipse/buildConfigs/sdk.tests/testConfigs/win32-5.0/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win32-5.0/testAll.bat
@@ -3,7 +3,7 @@ cd %1
 REM test script
 
 REM add the extra binaries to the system path
-set PATH=%PATH%;%1\..\windowsBin
+set PATH=%PATH%;%1\..\windowsBin;C:\WRSHDNT
 
 REM run all tests
 call runtests.bat -vm ..\jdk1.5.0_06\jre\bin\java -properties vm.properties > %2
