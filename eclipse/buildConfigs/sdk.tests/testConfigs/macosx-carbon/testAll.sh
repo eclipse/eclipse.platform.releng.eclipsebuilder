e06bf5fd6bce130bbbdd33a9da04eeb92ca7907f
Sonia Dimitrov <sdimitro>
2006-05-02 22:01:24 +0000

    remove path setting

--- eclipse/buildConfigs/sdk.tests/testConfigs/macosx-carbon/testAll.sh
+++ eclipse/buildConfigs/sdk.tests/testConfigs/macosx-carbon/testAll.sh
@@ -1,6 +1,5 @@
 # !/bin/sh
 ulimit -c unlimited
-PATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.4.2/Commands/:$PATH;export PATH
 
 #execute command to run tests
 ./runtests -vm "java -XstartOnFirstThread" -os macosx -ws carbon -arch ppc -properties vm.properties> macosx.carbon_consolelog.txt
