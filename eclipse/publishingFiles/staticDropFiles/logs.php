617eaa471b09bcbe8acd8de6a9dcbbff8dff3dc6
Kim Moir <kmoir>
2008-08-08 13:41:35 +0000

    Bug 242779 Start Cocoa builds

--- eclipse/publishingFiles/staticDropFiles/logs.php
+++ eclipse/publishingFiles/staticDropFiles/logs.php
@@ -236,13 +236,15 @@ echo " <li><a href=\"testresults/versiontool/results.xml\"><b> Versioning Compar
 echo "This log compares the build's plugin and features versions with 3.3. </li> ";
 }
 ?>
+/* 
 <?php
 echo " <li><a href=\"apitools/report.html\"><b>PDE API Tools Report</b></a>";
 echo "  This report describes API changes since M6a.  Exclusions are listed in org.eclipse.releng/apiexclude/exclude_list.txt. </li> ";
 ?>
+*/
 <?php
 echo " <li><a href=\"apitools/apitoolsverifications/html/index.html\"><b>PDE API Tools Verification Report</b></a>";
-echo "  This tool verifies the versions of the plugins against Eclipse 3.3.2.  Exclusions are listed in org.eclipse.releng/apiexclude/exclude_list_external.txt. </li> ";
+echo "  This tool verifies the versions of the plugins against Eclipse 3.4.  Exclusions are listed in org.eclipse.releng/apiexclude/exclude_list_external.txt. </li> ";
 ?>
 
 </li>
