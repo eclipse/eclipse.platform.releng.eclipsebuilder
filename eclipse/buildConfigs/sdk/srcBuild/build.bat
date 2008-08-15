aaa7c1aaaec56535d6e7cf87afa85067025fc12f
Kim Moir <kmoir>
2008-08-15 21:58:41 +0000

    revert Bug 242779 Start Cocoa builds

--- eclipse/buildConfigs/sdk/srcBuild/build.bat
+++ eclipse/buildConfigs/sdk/srcBuild/build.bat
@@ -47,7 +47,6 @@ if %installOs%-%ws%-%arch%==hpux-motif-PA_RISC goto run
 if %installOs%-%ws%-%arch%==qnx-photon-x86 goto run
 if %installOs%-%ws%-%arch%==hpux-motif-ia64_32 goto run
 if %installOs%-%ws%-%arch%==macosx-carbon-ppc goto run
-if %installOs%-%ws%-%arch%==macosx-cocoa-x86 goto run
 
 ECHO The ws os arch combination entered is not valid.
 goto end
