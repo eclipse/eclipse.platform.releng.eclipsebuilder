35a85486a43febc0e0397ceabeeddb6cfb9993cd
Kim Moir <kmoir>
2008-11-14 19:22:54 +0000

    Bug 245693 Need perf_34x branch and baselines from it

--- eclipse/buildConfigs/sdk.tests/testConfigs/win32xp-perf/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win32xp-perf/testAll.bat
@@ -8,5 +8,5 @@ REM add Cloudscape plugin to junit tests zip file
 zip eclipse-junit-tests-%3%.zip -rm eclipse
 
 REM run all tests
-call runtests.bat -vm %cd%\..\jdk1.5.0_14\jre\bin\javaw -properties vm.properties "-Dtest.target=performance" "-Dplatform=win32perf1" 1> %2 2>&1
+call runtests.bat -vm %cd%\..\jdk1.5.0_14\jre\bin\javaw -properties vm.properties "-Dtest.target=performance" "-Dplatform=win32.x86.perf1" 1> %2 2>&1
 exit
