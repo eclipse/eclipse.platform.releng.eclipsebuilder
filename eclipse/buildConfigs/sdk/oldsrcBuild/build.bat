f45bd07a6a65a5350799c121a0b4d9143c1300b1
Kim Moir <kmoir>
2009-07-23 13:35:55 +0000

    Bug 284290 change macosx.carbon.ppc drops to macosx.carbon.x86

--- eclipse/buildConfigs/sdk/oldsrcBuild/build.bat
+++ eclipse/buildConfigs/sdk/oldsrcBuild/build.bat
@@ -45,6 +45,7 @@ if %installOs%-%ws%-%arch%==hpux-motif-PA_RISC goto run
 if %installOs%-%ws%-%arch%==qnx-photon-x86 goto run
 if %installOs%-%ws%-%arch%==hpux-motif-ia64_32 goto run
 if %installOs%-%ws%-%arch%==macosx-carbon-ppc goto run
+if %installOs%-%ws%-%arch%==macosx-carbon-x86 goto run
 if %installOs%-%ws%-%arch%==macosx-cocoa-x86 goto run
 if %installOs%-%ws%-%arch%==macosx-cocoa-x86_64 goto run
 
