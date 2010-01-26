447cdf366d5fa990da93d2043ab3897778169694
Kim Moir <kmoir>
2010-01-26 15:25:38 +0000

    Bug 300691 -  equinox agent is failing to provision in 3.6 builds

--- bootstrapHudsonp2.sh
+++ bootstrapHudsonp2.sh
@@ -59,7 +59,8 @@ tag=""
 #buildProjectTags=v20100108
 #buildProjectTags=v20100113
 #buildProjectTags=v20100114
-buildProjectTags=v20100118a
+#buildProjectTags=v20100118a
+buildProjectTags=v20100126
 
 #updateSite property setting
 updateSite=""
