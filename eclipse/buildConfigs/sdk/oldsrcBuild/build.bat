aaffbf04111880e9dc315a28630d150951e2ca36
Kim Moir <kmoir>
2010-02-01 14:22:38 +0000

    Bug 293830 reduce the number of platforms in the build

--- eclipse/buildConfigs/sdk/oldsrcBuild/build.bat
+++ eclipse/buildConfigs/sdk/oldsrcBuild/build.bat
@@ -30,7 +30,6 @@ if x%installOs%==x goto usage
 if x%ws%==x goto usage 
 if x%arch%==x goto usage
 if %installOs%-%ws%-%arch%==win32-win32-x86 goto run
-if %installOs%-%ws%-%arch%==win32-wpf-x86 goto run
 if %installOs%-%ws%-%arch%==linux-motif-x86 goto run
 if %installOs%-%ws%-%arch%==linux-gtk-x86 goto run
 if %installOs%-%ws%-%arch%==linux-gtk-ppc goto run
