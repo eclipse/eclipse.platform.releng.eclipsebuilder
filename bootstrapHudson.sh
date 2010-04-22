894cb6832e1aad7f1b967990d9c62211767531d7
Kim Moir <kmoir>
2010-04-22 17:37:21 +0000

    Bug 310013 Upgrade to Jetty 6.1.23

--- bootstrapHudson.sh
+++ bootstrapHudson.sh
@@ -68,7 +68,8 @@ tag=""
 #buildProjectTags=v20100305
 #buildProjectTags=v20100312
 #buildProjectTags=v20100315
-buildProjectTags=v20100414a
+#buildProjectTags=v20100414a
+buildProjectTags=v20100422
 
 #updateSite property setting
 updateSite=""
