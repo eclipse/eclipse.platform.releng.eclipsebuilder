4c47bec1a42602aa8d1b64cbdc48dd3cc56005e0
Kim Moir <kmoir>
2010-09-22 19:23:25 +0000

    update version

--- bootstrapHudson.sh
+++ bootstrapHudson.sh
@@ -60,7 +60,7 @@ tag=""
 #buildProjectTags=v20100715
 #buildProjectTags=v20100719c
 #buildProjectTags=v20100907
-buildProjectTags=v201009022
+buildProjectTags=v20100922
 
 #updateSite property setting
 updateSite=""
