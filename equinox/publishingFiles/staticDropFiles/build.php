9289b0812d10dd8b5d31aee00c46b67fda66345d
Kim Moir <kmoir>
2010-10-08 19:12:24 +0000

    Bug 326444 -  Equinox download page format

--- equinox/publishingFiles/staticDropFiles/build.php
+++ equinox/publishingFiles/staticDropFiles/build.php
@@ -1,16 +1,8 @@
 <?php
 
-function column($servername) {
-      echo "<td width=\"7%\"><b>Download</b></td>";
-      echo "<td width=\"7%\"><b>Size</b></td>";
-      echo "<td width=\"40%\"><b>File</b></td>";
-      echo "<td width=\"11%\"><b>Checksum</b></td>";
-      return;
-}
-
 function generateDropSize($zipfile) {
 		$filesize = getDropSize($zipfile);
-		 echo "<td>$filesize</td>";
+		 return "<td>$filesize</td>";
 }
 
 function getDropSize($zipfile) {
@@ -18,29 +10,16 @@ function getDropSize($zipfile) {
 		 $filesizebytes  = filesize($zipfile);
 		 if($filesizebytes > 0) {
 			if($filesizebytes < 1048576)
-				$filesize = round($filesizebytes / 1048576, 2) . " MB";
+				$filesize = round($filesizebytes / 1024, 0) . "K";
 			else if ($filesizebytes >= 1048576 && $filesizebytes < 10485760)
-				$filesize = round($filesizebytes / 1048576, 1) . " MB";
+				$filesize = round($filesizebytes / 1048576, 1) . "M";
 			else
-        		 	$filesize = round($filesizebytes / 1048576, 0) . " MB";
+        		 	$filesize = round($filesizebytes / 1048576, 0) . "M";
 		 }
 		 return($filesize);
 }
 
-function generateChecksumLinks($zipfile) {
-     echo "<td><a href=\"http://download.eclipse.org/equinox/drops/@buildlabel@/checksum/$zipfile.md5\"><img src=\"md5.png\" alt=\"md5\"/></a>";
-     echo "<a href=\"http://download.eclipse.org/equinox/drops/@buildlabel@/checksum/$zipfile.sha1\"><img src=\"sha1.png\" alt=\"sha1\"/></a></td>";
-}
-
-function genLinks($servername,$buildlabel,$zipfile) {
-     $httplink = "download.php?dropFile=$zipfile";
-     $httplabel = "(http)";
-     $httpline =  "<div align=\"left\"><a href=\"$httplink\">$httplabel</a>";
-     echo "$httpline";
-     echo "&nbsp;&nbsp";
-     generateDropSize($zipfile);
-     echo "<td>$zipfile</td>";
-     echo "<td><a href=\"http://download.eclipse.org/equinox/drops/@buildlabel@/checksum/$zipfile.md5\">(md5)</a>";
-     echo " <a href=\"http://download.eclipse.org/equinox/drops/@buildlabel@/checksum/$zipfile.sha1\">(sha1)</a></td>";
+function generateChecksumLinks($zipfile, $buildlabel) {
+     return "<td><a href=\"http://download.eclipse.org/equinox/drops/$buildlabel/checksum/$zipfile.md5\"><img src=\"/equinox/images/md5.png\" alt=\"md5\"/></a><a href=\"http://download.eclipse.org/equinox/drops/$buildlabel/checksum/$zipfile.sha1\"><img src=\"/equinox/images/sha1.png\" alt=\"sha1\"/></a></td>";
 }
 ?>
