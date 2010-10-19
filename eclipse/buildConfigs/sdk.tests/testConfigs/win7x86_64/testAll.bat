0fb901838e31118adac22cb256e9712a904542d8
Kim Moir <kmoir>
2010-10-19 20:49:18 +0000

    Bug 295393 -  create Hudson build to run JUnit tests on one platform

--- eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/testAll.bat
@@ -7,6 +7,6 @@ REM set PATH=%PATH%;%executionDir\..\windowsBin
 
 REM run all tests
 REM call runtests.bat -vm \"c:\Program Files\Java\jdk1.6.0_20\jre\bin\javaw\" -properties vm.properties > error.log
-runtests.bat -properties vm.properties > error.log
+runtests.bat -os win32 -ws win32 -arch x86_64 -properties vm.properties > error.log
 
 exit
