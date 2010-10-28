f482663e26099346def281149de9173a83cb729f
cvs2svn <platform-releng-dev@eclipse.org>
2010-10-28 21:05:34 +0000

    This commit was manufactured by cvs2svn to create tag 'v20101028test'.
    Sprout from master 2010-10-28 21:05:33 UTC Kim Moir <kmoir> ' Bug 295393 -  create Hudson build to run JUnit tests on one platform'
    Cherrypick from master 2010-10-28 18:44:10 UTC Kim Moir <kmoir> ' Bug 295393 -  create Hudson build to run JUnit tests on one platform':
        eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/customTest.xml
        eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/equinoxp2tests.properties
        eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/testAll.bat
        eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/testing.properties
        eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/vm.properties

---
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7x86_64/testAll.bat
@@ -0,0 +1,12 @@
+@echo on
+cd %executionDir
+REM test script
+
+REM add the extra binaries to the system path
+REM set PATH=%PATH%;%executionDir\..\windowsBin
+
+REM run all tests
+REM runtests.bat -os win32 -ws win32 -arch x86_64 -vm c:\\java\\jdk1.6.0_20\\jre\\bin\\javaw -properties vm.properties > error.log
+runtests.bat -os win32 -ws win32 -arch x86 -vm c:\\java\\jdk1.6.0_20\\jre\\bin\\javaw -properties vm.properties > win7.log
+
+exit
