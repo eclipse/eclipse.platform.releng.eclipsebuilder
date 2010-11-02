93913e9d2fd529b65eef8aec5280da9647cb91b1
Kim Moir <kmoir>
2010-11-02 20:06:04 +0000

    Bug 295393 -  create Hudson build to run JUnit tests on one platform

--- eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
+++ eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
@@ -17,4 +17,5 @@ ls -la runtests
 #execute command to run tests
 /bin/chmod 755 runtests
 ls -la runtests
+ls -la /shared/common/jdk-1.6.x86_64/bin/java
 /bin/bash runtests -os linux -ws gtk -arch x86_64 -vm /shared/common/jdk-1.6.x86_64/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt 
