59e6d781c9f6c0c89505b896abfe74c94412c2d3
Kim Moir <kmoir>
2010-11-04 18:22:49 +0000

    Bug 295393 -  create Hudson build to run JUnit tests on one platform

--- eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
+++ eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 cd .
 #environment variables
-echo localhost > auth.cfg
-Xvfb :8 -screen 0 1280x1024x24 -auth auth.cfg &
-#DISPLAY=$HOSTNAME:0.0
-DISPLAY=:8.0 metacity --display=:8.0 --replace --sm-disable >/dev/null 2>&1 &
 PATH=$PATH:`pwd`/../linux;export PATH
 #xhost +$HOSTNAME
-MOZILLA_FIVE_HOME=/usr/lib/firefox-1.5.0.12; export MOZILLA_FIVE_HOME
+MOZILLA_FIVE_HOME=/usr/lib/mozilla-1.7.12;export MOZILLA_FIVE_HOME
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MOZILLA_FIVE_HOME
-#USERNAME=`whoami`
+USERNAME=`whoami`
+#DISPLAY=$HOSTNAME:0.0
 ulimit -c unlimited
 
 export DISPLAY LD_LIBRARY_PATH
-echo DISPLAY DISPLAY
+
 ls -la runtests.sh
 #execute command to run tests
 /bin/chmod 755 runtests.sh
