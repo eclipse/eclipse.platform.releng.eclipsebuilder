28897d8a2c137973011b2a51f27f98e04536c7d9
Kim Moir <kmoir>
2010-11-09 19:03:24 +0000

    NEW - bug 329828: Remove Photon SWT builds? https://bugs.eclipse.org/bugs/show_bug.cgi?id=329828

--- bootstrapHudson.sh
+++ bootstrapHudson.sh
@@ -52,7 +52,8 @@ tag=""
 #buildProjectTags=v20101019
 #buildProjectTags=v20101027c
 #buildProjectTags=v20101029
-buildProjectTags=v20101001
+#buildProjectTags=v20101001
+buildProjectTags=v20101109
 
 #updateSite property setting
 updateSite=""
