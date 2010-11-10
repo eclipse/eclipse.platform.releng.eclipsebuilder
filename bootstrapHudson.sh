23dcc6a7479e7befedeadcd7024efbed7921b423
Kim Moir <kmoir>
2010-11-10 20:49:52 +0000

    NEW - bug 329722: new features and products https://bugs.eclipse.org/bugs/show_bug.cgi?id=329722

--- bootstrapHudson.sh
+++ bootstrapHudson.sh
@@ -53,7 +53,7 @@ tag=""
 #buildProjectTags=v20101027c
 #buildProjectTags=v20101029
 #buildProjectTags=v20101001
-buildProjectTags=v20101109
+buildProjectTags=v20101110
 
 #updateSite property setting
 updateSite=""
