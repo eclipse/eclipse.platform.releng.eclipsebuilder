3cd82be9067f72b8e81ea2b42cdfcdada6512d70
Kim Moir <kmoir>
2010-11-24 21:26:34 +0000

    NEW - bug 295393: create Hudson build to run JUnit tests on one platform https://bugs.eclipse.org/bugs/show_bug.cgi?id=295393

--- eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
+++ eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
@@ -3,5 +3,6 @@ cd .
 export MOZILLA_FIVE_HOME=/usr/lib64/xulrunner-1.9.0.6;export MOZILLA_FIVE_HOME
 
 /bin/chmod 755 runtests.sh
-./runtests.sh -os linux -ws gtk -arch x86_64 -vm /shared/common/jdk-1.6.x86_64/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
+#./runtests.sh -os linux -ws gtk -arch x86_64 -vm /shared/common/jdk-1.6.x86_64/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
+./runtests.sh -os linux -ws gtk -arch x86 -vm /shared/common/jdk-1.6.0_10/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
 exit 
