28a25f8f9861bc23000f99dd3dbef7e07d570fc9
Kim Moir <kmoir>
2010-12-21 17:14:25 +0000

    NEW - bug 295393: create Hudson build to run JUnit tests on one platform https://bugs.eclipse.org/bugs/show_bug.cgi?id=295393

--- bootstrapHudson.sh
+++ bootstrapHudson.sh
@@ -217,7 +217,7 @@ echo BUILD_WORKSPACE=$WORKSPACE > $WORKSPACE/test.properties
 echo BUILD_JOB_NAME=$JOB_NAME >> $WORKSPACE/test.properties
 echo BUILD_BUILD_NUMBER=$BUILD_NUMBER >> $WORKSPACE/test.properties
 echo buildId=$buildId >> $WORKSPACE/test.properties
-echo org.eclipse.equinox.p2.tests.current.build.repo=https://hudson.eclipse.org/hudson/job/$JOB_NAME/$BUILD_NUMBER/artifact/builds/transfer/files/repo/$buildId >> $WORKSPACE/test.properties 
+echo currentbuildrepo=https://hudson.eclipse.org/hudson/job/$JOB_NAME/$BUILD_NUMBER/artifact/builds/transfer/files/repo/$buildId >> $WORKSPACE/test.properties 
 
 #the base command used to run AntRunner headless
 buildMachineArch=`uname -p`
