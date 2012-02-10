96fe20700679aa02ad6fa5ead28add2c6893edb5
Kim Moir <kmoir>
2012-02-10 18:04:07 +0000

    Bug 355430 - change primary builder to 4.2

--- eclipse/buildConfigs/sdk.tests/testConfigs/macmini/testAll.sh
+++ eclipse/buildConfigs/sdk.tests/testConfigs/macmini/testAll.sh
@@ -4,4 +4,5 @@ ulimit -c unlimited
 #execute command to run tests
 /bin/chmod 755 runtests.sh
 ./runtests.sh -os macosx -ws cocoa -arch x86 -properties vm.properties > macosx.cocoa_consolelog.txt
+echo done > testsdone.txt
 exit
