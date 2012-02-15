55c32c032b6a225153e7164ca0badfa9fededd79
Kim Moir <kmoir>
2012-02-15 15:49:15 +0000

    Bug 355430 - change primary builder to 4.2

--- eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
+++ eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
@@ -4,6 +4,6 @@ cd .
 export MOZILLA_FIVE_HOME=/usr/lib/xulrunner-1.9.0.19;export MOZILLA_FIVE_HOME
 
 /bin/chmod 755 runtests.sh
-#./runtests.sh -os linux -ws gtk -arch x86_64 -vm /shared/common/jdk-1.6.x86_64/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
-./runtests.sh -os linux -ws gtk -arch x86 -vm /shared/common/jdk-1.6.0_10/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
+./runtests.sh -os linux -ws gtk -arch x86_64 -vm /shared/common/jdk-1.6.x86_64/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
+#./runtests.sh -os linux -ws gtk -arch x86 -vm /shared/common/jdk-1.6.0_10/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
 exit 
