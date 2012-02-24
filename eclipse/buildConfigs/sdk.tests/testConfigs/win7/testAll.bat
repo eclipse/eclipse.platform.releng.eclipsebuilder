66f2ca4353054598047248eb0c6001d8ed717686
Kim Moir <kmoir>
2012-02-24 18:42:37 +0000

    Bug 355430 - change primary builder to 4.2

--- eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
@@ -7,6 +7,6 @@ REM set PATH=%PATH%;%executionDir\..\windowsBin
 
 REM run all tests
 REM runtests.bat -os win32 -ws win32 -arch x86_64 -vm c:\\java\\jdk1.6.0_20\\jre\\bin\\javaw -properties vm.properties > error.log
-runtests.bat -os win32 -ws win32 -arch x86 -vm c:\\java\\jdk1.6.0_20\\jre\\bin\\javaw -properties vm.properties > win7.log
+runtests.bat -os win32 -ws win32 -arch x86 -vm c:\\java\\jdk7u2\\jre\\bin\\javaw -properties vm.properties > win7.log
 
 exit
