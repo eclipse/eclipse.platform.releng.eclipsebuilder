3d4050b6cf5ee4a774c5b1037679a8a82ecb6dc4
David Williams <david_williams@us.ibm.com>
2012-04-02 16:14:02 -0400

    Add error checking

--- scripts/masterBuild.sh
+++ scripts/masterBuild.sh
@@ -203,6 +203,32 @@ buildDirEclipse="$buildDir/eclipse"
 WORKSPACE="$buildDir/workspace"
 export WORKSPACE
 
+function checkForErrorExit ()
+{
+    # arg 1 must be return code, $?
+    # arg 2 (remaining line) can be message to print before exiting do to non-zero exit code
+    exitCode=$1
+    shift
+    message="$*"
+    if [ -z "${exitCode}" ]
+    then
+        echo "PROGRAM ERROR: checkForErrorExit called with no arguments"
+        exit 1
+    fi
+    if [ -z "${message}" ]
+    then
+        echo "WARNING: checkForErrorExit called without message"
+        message="(Calling program provided no message)"
+    fi
+    if [ "${exitCode}" -ne "0" ]
+    then
+        echo
+        echo "   ERROR. exit code: ${exitCode}"  ${message}
+        echo
+        exit "${exitCode}"
+    fi
+}
+
 
 # first, let's check out all of those pesky projects
 updateBaseBuilder () {
@@ -752,7 +778,9 @@ tagRepo () {
 
 updateBaseBuilder
 updateBaseBuilderInfo
+
 updateEclipseBuilder
+checkForErrorExit $? "Failed while updating Eclipse Buidler"
 
 #tagRepo
 
