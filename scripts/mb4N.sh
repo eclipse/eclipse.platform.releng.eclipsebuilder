0217143bd02644cb672934c995817bbf07c9dea6
David Williams <david_williams@us.ibm.com>
2012-04-14 00:36:17 -0400

    prep for cron job

--- scripts/mb4N.sh
+++ scripts/mb4N.sh
@@ -5,15 +5,20 @@
 # but the steps of getting those key files and directories are repeated here.
 # This basically bootstraps the files fresh each time (after the first)
 # though of course, the mb4NCaptureOutput.sh won't be used until the next time.
-buildRoot=/shared/eclispe/eclipse4N 
+
+# path required when starting from cron job
+export PATH=/usr/local/bin:/usr/bin:/bin:
+
+buildRoot=/shared/eclipse/eclipse4N 
 mkdir -p $buildRoot
 cd $buildRoot
+
+date >> job4nstarted.txt
+
 wget -O mb4N.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/mb4N.sh?h=master;
 wget -O masterBuild.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/masterBuild.sh?h=master;
 
 chmod -c +x *.sh
-
-date >> job4nstarted.txt
 
 # Debug version
 DEBUG=true $buildRoot/masterBuild.sh -buildType N -eclipseStream 4.2 -buildRoot $buildRoot  2>&1 | tee fullmasterBuildOutput.txt
