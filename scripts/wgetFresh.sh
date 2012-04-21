5d1fd96b99cdcef9510052b538afbf1466aead66
David Williams <david_williams@us.ibm.com>
2012-04-21 01:24:38 -0400

    improveScripts

--- scripts/wgetFresh.sh
+++ scripts/wgetFresh.sh
@@ -14,19 +14,19 @@ branchOrTag=master
 # to build, all that's needed is the appropriate mbNX.sh scripts. It gets what ever 
 # else it needs. 
 
-wget -O masterBuild.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/masterBuild.sh?h=$branchOrTag;
-wget -O mb3I.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/mb3I.sh?h=$branchOrTag;
-wget -O mb4I.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/mb4I.sh?h=$branchOrTag;
-wget -O mb4N.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/mb4N.sh?h=$branchOrTag;
+wget --no-verbose -O masterBuild.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/masterBuild.sh?h=$branchOrTag 2>&1;
+wget --no-verbose -O mb3I.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/mb3I.sh?h=$branchOrTag 2>&1;
+wget --no-verbose -O mb4I.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/mb4I.sh?h=$branchOrTag 2>&1;
+wget --no-verbose -O mb4N.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/mb4N.sh?h=$branchOrTag 2>&1;
 
 
 # handy script to "wrap" a normal build script such as mb4I.sh to set global test/debug settings
-wget -O testBuild.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/testBuild.sh?h=$branchOrTag;
+wget --no-verbose -O testBuild.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/testBuild.sh?h=$branchOrTag 2>&1;
 
 # get this script itself (would have to run twice to make use changes, naturally)
 # and has trouble "writing over itself" so we put in a file with 'NEW' suffix
 # and a command line like the following works well
 # ./wgetFresh.sh ; mv wgetFresh.shNEW wgetFresh.sh
-wget -O wgetFresh.shNEW http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/wgetFresh.sh?h=$branchOrTag;
+wget -O --no-verbose wgetFresh.shNEW http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/wgetFresh.sh?h=$branchOrTag 2>&1;
 
 chmod +x *.sh
