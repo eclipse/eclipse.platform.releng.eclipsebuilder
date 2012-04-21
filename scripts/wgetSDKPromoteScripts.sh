be2f8ae72cea744f1189805495d4f190c2e8cc7e
David Williams <david_williams@us.ibm.com>
2012-04-21 00:57:00 -0400

    improve promote scripts

--- scripts/wgetSDKPromoteScripts.sh
+++ scripts/wgetSDKPromoteScripts.sh
@@ -9,9 +9,10 @@ branchOrTag=master
 # remember that wget puts most its output on "standard error", I assume 
 # following the philosophy that anything "diagnosic" related goes to standard err, 
 # and only things the user really needs wants to see as "results" goes to standard out
+# but in cron jobs and similar, this comes across as "an error".
 
-wget -O syncDropLocation.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/syncDropLocation.sh?h=$branchOrTag 2>/dev/null
-wget -O sdkPromotionCronJob.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/sdkPromotionCronJob.sh?h=$branchOrTag 2/dev/null
-wget -O wgetSDKPromoteScriptsNEW.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/wgetSDKPromoteScripts.sh?h=$branchOrTag 2/dev/null
+wget --no-verbose -O syncDropLocation.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/syncDropLocation.sh?h=$branchOrTag 2>&1
+wget --no-verbose -O sdkPromotionCronJob.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/sdkPromotionCronJob.sh?h=$branchOrTag 2>&1
+wget --no-verbose -O wgetSDKPromoteScripts.NEW.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.eclipsebuilder.git/plain/scripts/wgetSDKPromoteScripts.sh?h=$branchOrTag 2>&1
 
 chmod +x *.sh
