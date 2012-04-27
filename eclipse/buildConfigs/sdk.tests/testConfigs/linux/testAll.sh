df9055b5e42adabf413fc18501debeb1423cba91
David Williams <david_williams@us.ibm.com>
2012-04-27 01:58:26 -0400

    377365 - get unit tests running (more attempts to over come hard coded VM)

--- eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
+++ eclipse/buildConfigs/sdk.tests/testConfigs/linux/testAll.sh
@@ -1,9 +1,29 @@
-#!/bin/sh
-cd .
-#export MOZILLA_FIVE_HOME=/usr/lib64/xulrunner-1.9.0.6;export MOZILLA_FIVE_HOME
-export MOZILLA_FIVE_HOME=/usr/lib/xulrunner-1.9.0.19;export MOZILLA_FIVE_HOME
+#!/usr/bin/env bash
+
+echo "USER: $USER"
+echo "PATH: $PATH"
+# This file should never exist or be needed for production machine, 
+# but allows an easy way for a "local user" to provide this file 
+# somewhere on the search path ($HOME/bin is common), 
+# and it will be included here, thus can provide "override values" 
+# to those defined by defaults for production machine., 
+# such as for vmcmd
+
+source localTestsProperties.shsource
+
+echo "PWD: $PWD"
+vmcmd=${vmcmd:-/shared/common/jdk-1.6.x86_64/jre/bin/java}
+
+echo "vmcmd: $vmcmd"
+
+export MOZILLA_FIVE_HOME=${MOZILLA_FIVE_HOME:-/usr/lib/xulrunner-1.9.0.19}
+
+echo "MOZILLA_FIVE_HOME: ${MOZILLA_FIVE_HOME}"
+
+eclipseArch=${eclipseArch:-x86_64}
 
 /bin/chmod 755 runtests.sh
-./runtests.sh -os linux -ws gtk -arch x86_64 -vm /shared/common/jdk-1.6.x86_64/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
-#./runtests.sh -os linux -ws gtk -arch x86 -vm /shared/common/jdk-1.6.0_10/jre/bin/java -properties vm.properties > linux.gtk-6.0_consolelog.txt
-exit 
+
+./runtests.sh -os linux -ws gtk -arch $eclipseArch -vm "${vmcmd}" -properties vm.properties > linux.gtk-6.0_consolelog.txt
+
+
