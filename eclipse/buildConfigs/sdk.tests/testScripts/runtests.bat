91cb2602122fc7cc54deb25e547ab8db6ca2649e
David Williams <david_williams@us.ibm.com>
2012-04-27 05:05:07 -0400

    Bug 377859 - Default timeout of 7200000 ms is too high? (set to 15 minute timeout, for now)

--- eclipse/buildConfigs/sdk.tests/testScripts/runtests.bat
+++ eclipse/buildConfigs/sdk.tests/testScripts/runtests.bat
@@ -56,7 +56,7 @@ REM get name of org.eclipse.equinox.launcher_*.jar with version label
 dir /b eclipse\plugins\org.eclipse.equinox.launcher_*.jar>launcher-jar-name.txt
 set /p launcher-jar=<launcher-jar-name.txt
 
-%vmcmd% -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch% -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
+%vmcmd% -Dtimeout=900000 -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch% -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
 
 goto end
 
