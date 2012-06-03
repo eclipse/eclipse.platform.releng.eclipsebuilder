b997d232331168367474917457839c6bbf3697cf
David Williams <david_williams@us.ibm.com>
2012-06-03 11:57:05 -0400

    Bug 372880 - hudson windows slave needs to be able to ... (revert experiments)

--- eclipse/buildConfigs/sdk.tests/testScripts/runtests.bat
+++ eclipse/buildConfigs/sdk.tests/testScripts/runtests.bat
@@ -55,10 +55,14 @@ REM ***************************************************************************
 REM get name of org.eclipse.equinox.launcher_*.jar with version label
 dir /b eclipse\plugins\org.eclipse.equinox.launcher_*.jar>launcher-jar-name.txt
 set /p launcher-jar=<launcher-jar-name.txt
-set /p http.nonProxyHosts="127.0.0.1|localhost|*.localhost|local|*.local|169.254/16|*.169.254/16|eclipse.org|*.eclipse.org|hudson.eclipse.org|*.hudson.eclipse.org|dev.eclipse.org|*.dev.eclipse.org"
+
+rem tried using these as experiements for bug 372880
+rem https://bugs.eclipse.org/bugs/show_bug.cgi?id=372880
+rem set /p http.nonProxyHosts="127.0.0.1|localhost|*.localhost|local|*.local|169.254/16|*.169.254/16|eclipse.org|*.eclipse.org|hudson.eclipse.org|*.hudson.eclipse.org|dev.eclipse.org|*.dev.eclipse.org"
+rem -Dhttp.nonProxyHosts=%http.nonProxyHosts%
 
 rem -Dtimeout=1800000
-%vmcmd% -Dhttp.nonProxyHosts=%http.nonProxyHosts% -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch% -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
+%vmcmd% -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch% -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
 
 goto end
 
