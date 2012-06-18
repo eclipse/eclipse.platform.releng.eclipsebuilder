94d2dc0110fe8396051b5d06d266b09f2fd3067e
David Williams <david_williams@us.ibm.com>
2012-06-18 16:24:01 -0400

    Bug 208422 - Eclipse project download page needs love

--- downloadsites/eclipse/downloads/eclipse3x.php
+++ downloadsites/eclipse/downloads/eclipse3x.php
@@ -1,20 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<?php
-//ini_set("display_errors", "true");
-//error_reporting (E_ALL);
+<?php   
+  # Begin: page-specific settings.  Change these. 
+  $pageTitle    = "Eclipse Project Downloads";
+  $pageKeywords = "";
+  $pageAuthor   = "";
+  
+  //ini_set("display_errors", "true");
+  //error_reporting (E_ALL);
 $eclipseStream="3";
 $otherIndexFile="index.html";
 $otherStream="4";
 include('dlconfig3.php');
 $subdirDrops="drops";
 
-?>
+  # Use the basic white layout if the file is not hosted on download.eclipse.org
+  $layout = (array_key_exists("SERVER_NAME", $_SERVER) && ($_SERVER['SERVER_NAME'] == "download.eclipse.org")) ? "default" : "html";
+  
+  ob_start();
+
+  switch($layout){
+    case 'html':
+      #If this file is not on download.eclipse.org print the legacy headers.?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" href="../default_style.css" />
-<title>Eclipse Project Downloads</title></head>
-<body>
+<title><?php echo $pageTitle;?></title></head>
+<body><?php 
+      break;    
+    default:
+      #Otherwise use the default layout (content printed inside the nova theme).
+      require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+      require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+      require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+      $App  = new App();
+      $Nav  = new Nav();
+      $Menu   = new Menu();
+      break;      
+  }?>
+<div class="container_<?php echo $layout;?>">
 <table border="0" cellspacing="5" cellpadding="2" width="100%" > 
 
 <tr> 
@@ -44,14 +68,14 @@ Latest downloads from the Eclipse project
 <td align="left" valign="top" colspan="2" bgcolor="#0080C0"><font color="#FFFFFF" face="Arial,Helvetica">Latest 
 Downloads</font></td></tr> <!-- The Eclipse Projects --> <tr> <td> 
 <p>On this
-page you can find the latest <a href="build_types.html" target="_top">builds</a> produced by
-the <a href="http://www.eclipse.org/eclipse" target="_top">Eclipse
-Project</a>. To get started run the program and go through the user and developer
+page you can find the latest <a href="build_types.html" target="_top">builds</a> produced by 
+the <a href="http://www.eclipse.org/eclipse" target="_top">Eclipse 
+Project</a>. To get started run the program and go through the user and developer 
 documentation provided in the online help system. If you have problems downloading
 the drops, contact the <font size="-1" face="arial,helvetica,geneva"><a href="mailto:webmaster@eclipse.org">webmaster</a></font>.
-If you have problems installing or getting the workbench to run, <a href="http://wiki.eclipse.org/index.php/The_Official_Eclipse_FAQs" target="_top">check
-out the Eclipse Project FAQ,</a> or try posting a question to the <a href="http://www.eclipse.org/newsgroups" target="_top">newsgroup</a>.
-All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.php" target="_top">Eclipse Foundation
+If you have problems installing or getting the workbench to run, <a href="http://wiki.eclipse.org/index.php/The_Official_Eclipse_FAQs" target="_top">check 
+out the Eclipse Project FAQ,</a> or try posting a question to the <a href="http://www.eclipse.org/newsgroups" target="_top">newsgroup</a>. 
+All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.php" target="_top">Eclipse Foundation 
 Software User Agreement</a> unless otherwise specified. </p>
 
 
@@ -71,7 +95,7 @@ builds</a>, access <a href="http://archive.eclipse.org/eclipse/downloads/">archi
 <?php
 
 function startsWithDropPrefix($dirName, $dropPrefix)
-{ 
+{  
 
     $result = false;
     // sanity check "setup" is as we expect
@@ -80,7 +104,7 @@ function startsWithDropPrefix($dirName, $dropPrefix)
         if (isset($dirName) && strlen($dirName) > 0) {
             $firstChar = substr($dirName, 0, 1);
             //echo "first char: ".$firstChar;
-            foreach($dropPrefix as $type) { 
+            foreach($dropPrefix as $type) {  
                 if ($firstChar == "$type") {
                     $result = true;
                     break;
@@ -115,7 +139,6 @@ function runTestBoxes($buildName, $testResultsDirName) {
     }
     return $boxes;
 }
-
 function printBuildColumns($fileName, $parts) {
     global $subdirDrops;
     // no file name, write empty column
@@ -140,10 +163,10 @@ function printBuildColumns($fileName, $parts) {
     $time=intval(date("H"))*60+intval(date("i"));
     $diff=($day-$buildDay)*24*60+$time-$buildTime;
     // Add icons
-    echo "<td valign=\"baseline\" >\n";
+    echo "<td valign=\"baseline\">\n";
     // hard code for now the build is done
     // https://bugs.eclipse.org/bugs/show_bug.cgi?id=378706
-    // but later, changed ... 
+    // but later, changed ...
     // compute build done based on "buildPending" file, but if not 
     // present, assume build is done
     // https://bugs.eclipse.org/bugs/show_bug.cgi?id=382196
@@ -152,6 +175,9 @@ function printBuildColumns($fileName, $parts) {
         $build_done=false;
     }
     if ($build_done) {
+        // test results location changed. 'testresults' is new standard
+        // but we check for 'results' for older stuff.
+        // https://bugs.eclipse.org/bugs/show_bug.cgi?id=379408
         $testResultsDirName="results";
         if (file_exists("$dropDir/testresults")) {
             $testResultsDirName="testresults";
@@ -232,7 +258,7 @@ function printBuildColumns($fileName, $parts) {
     echo "</td>\n";
     return $buildName;
 }
-?>
+?> 
 <?php
 // this is the main data computation part
 $aDirectory = dir($subdirDrops);
@@ -305,6 +331,7 @@ while ($anEntry = $aDirectory->read()) {
 </tr>
 <?php
 foreach($dropType as $value) {
+
     $prefix=$typeToPrefix[$value];
     // if empty bucket, do not print this row
     if (array_key_exists($prefix,$buckets)) {
@@ -338,7 +365,7 @@ foreach($dropType as $value) {
         }
     }
 }
-?>
+?> 
     </table></td></tr></table>
 
 
@@ -411,7 +438,22 @@ foreach($dropType as $value) {
     }
 }
 
-?>
-</body>
-</html>
+echo '</div>';
+$html = ob_get_clean();
+
+switch($layout){
+    case 'html':
+      #echo the computed content with the body and html closing tag. This is for the old layout.
+      echo $html;
+      echo '</body>';
+      echo '</html>';
+      break;
+        
+    default:
+      #For the default view we use $App->generatePage to generate the page inside nova.
+      $App->AddExtraHtmlHeader('<link rel="stylesheet" href="../default_style.css" />');
+      $App->Promotion = FALSE;
+      $App->generatePage('Nova', $Menu, NULL , $pageAuthor, $pageKeywords, $pageTitle, $html);
+      break;    
+}
 
