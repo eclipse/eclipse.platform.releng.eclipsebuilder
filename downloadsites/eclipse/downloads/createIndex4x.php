a7c8cb4b1e9606924f3ac77ee7fec1b1c9b7b733
David Williams <david_williams@us.ibm.com>
2012-06-18 13:23:21 -0400

    Bug 208422 - Eclipse project download page needs love

--- downloadsites/eclipse/downloads/createIndex4x.php
+++ downloadsites/eclipse/downloads/createIndex4x.php
@@ -1,20 +1,44 @@
+<?php   
+  # Begin: page-specific settings.  Change these. 
+  $pageTitle    = "Eclipse Project Downloads";
+  $pageKeywords = "";
+  $pageAuthor   = "";
+  
+  //ini_set("display_errors", "true");
+  //error_reporting (E_ALL);
+  $eclipseStream="4";
+  $otherIndexFile="eclipse3x.html";
+  $otherStream="3";
+  include('dlconfig4.php');
+  $subdirDrops="drops4";
+ 
+  # Use the basic white layout if the file is not hosted on download.eclipse.org
+  $layout = (array_key_exists("SERVER_NAME", $_SERVER) && ($_SERVER['SERVER_NAME'] == "download.eclipse.org")) ? "default" : "html";
+  
+  ob_start();
+
+  switch($layout){
+    case 'html':
+      #If this file is not on download.eclipse.org print the legacy headers.?> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<?php
-//ini_set("display_errors", "true");
-//error_reporting (E_ALL);
-$eclipseStream="4";
-$otherIndexFile="eclipse3x.html";
-$otherStream="3";
-include('dlconfig4.php');
-$subdirDrops="drops4";
-
-?>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" href="../default_style.css" />
-<title>Eclipse Project Downloads</title></head>
-<body>
+<title><?php echo $pageTitle;?></title></head>
+<body><?php 
+      break;    
+    default:
+      #Otherwise use the default layout (content printed inside the nova theme).
+      require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+      require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+      require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+      $App  = new App();
+      $Nav  = new Nav();
+      $Menu   = new Menu();
+      break;      
+  }?>
+<div class="container_<?php echo $layout;?>">
 <table border="0" cellspacing="5" cellpadding="2" width="100%" > 
 
 <tr> 
@@ -414,7 +438,22 @@ foreach($dropType as $value) {
     }
 }
 
-?>  
-</body>
-</html>
+echo '</div>';
+$html = ob_get_clean();
+
+switch($layout){
+    case 'html':
+      #echo the computed content with the body and html closing tag. This is for the old layout.
+      echo $html;
+      echo '</body>';
+      echo '</html>';
+      break;
+      	
+	default:
+      #For the default view we use $App->generatePage to generate the page inside nova.
+	  $App->AddExtraHtmlHeader('<link rel="stylesheet" href="../default_style.css" />');
+	  $App->Promotion = FALSE;
+	  $App->generatePage('Nova', $Menu, NULL , $pageAuthor, $pageKeywords, $pageTitle, $html);
+	  break;	
+}
 
