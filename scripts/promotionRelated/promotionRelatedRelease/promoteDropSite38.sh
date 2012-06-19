cd70d08ab2c52bd3c948bc7858d8e6f7695b39b0
David Williams <david_williams@us.ibm.com>
2012-06-18 23:04:03 -0400

    prep for Juno Release

--- scripts/promotionRelated/promotionRelatedRelease/promoteDropSite38.sh
+++ scripts/promotionRelated/promotionRelatedRelease/promoteDropSite38.sh
@@ -1,35 +1,10 @@
 #!/usr/bin/env bash
 
-DROP_ID=I20120608-1200
+# BUILD_ID is original I (or M) build on build machine that is being promoted
+BUILD_ID=I20120608-1200
+# $DL_LABEL takes form of 3.8M7, 3.8RC4, 3.8, etc.
 DL_LABEL=3.8
-BUILD_TIMESTAMP=${DROP_ID//[I-]/}
+# DROP_TYPE either S, R
+DROP_TYPE=R
 
-# Note 'R' for Release
-DL_DROP_ID=R-${DL_LABEL}-${BUILD_TIMESTAMP}
-
-source updateIndexFilesFunction.sh
-
-DL_SITE_PATH=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops/
-
-cd /opt/public/eclipse/eclipse3I/siteDir/eclipse/downloads/drops
-echo "PWD: ${PWD}"
-cp /opt/public/eclipse/sdk/renameBuild.sh .
-
-echo "save temp backup copy to ${DROP_ID}ORIG"
-rsync -ra ${DROP_ID}/ ${DROP_ID}ORIG
-
-echo "rename ${DROP_ID} ${DL_DROP_ID} ${DL_LABEL}"
-./renameBuild.sh ${DROP_ID} ${DL_DROP_ID} ${DL_LABEL}
-touch ${DL_DROP_ID}/buildHidden
-
-echo "rsync ${DL_DROP_ID} to ${DL_SITE_PATH}"
-rsync -r ${DL_DROP_ID} ${DL_SITE_PATH}
-
-# no update of index, with "buildHidden" file in place, should not show up
-#updateIndex 3
-
-echo "move backup back to original"
-mv ${DROP_ID}ORIG ${DROP_ID}
-
-rm renameBuild.sh 
-
+./promoteToDropSite ${BUILD_ID} ${DL_LABEL} ${DROP_TYPE}
--- scripts/promotionRelated/promotionRelatedRelease/promoteRepo42.sh
+++ scripts/promotionRelated/promotionRelatedRelease/promoteRepo42.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 
+BUILD_ID=I20120608-1400
+# DL_LABEL takes form of 3.8M7, 3.8RC4, 3.8, etc.
+DL_LABEL=4.2
+# DROP_TYPE either S, R
+DROP_TYPE=R
 
-DROP_SITE_ID=I20120608-1400
-
-DROP_LABEL=R-4.2
-
-BUILD_TIMESTAMP=${DROP_SITE_ID//[I-]/}
-
-DL_SITE_ID="${DROP_LABEL}"-"${BUILD_TIMESTAMP}"
-
-BUILDMACHINE_BASE_SITE=/opt/public/eclipse/eclipse4I/siteDir/updates/4.2-I-builds
-
-DLMACHINE_BASE_SITE=/home/data/httpd/download.eclipse.org/eclipse/updates/4.2
-
-BUILDMACHINE_SITE=${BUILDMACHINE_BASE_SITE}/${DROP_SITE_ID}
-
-DLMACHINE_SITE=${DLMACHINE_BASE_SITE}/${DL_SITE_ID}
-
-# remember, need trailing slash since going from existing directories 
-# contents to new directories contents
-echo "BUILDMACHINE_SITE: ${BUILDMACHINE_SITE}/"
-echo "DLMACHINE_SITE: ${DLMACHINE_SITE}"
-rsync -vr "${BUILDMACHINE_SITE}/"  "${DLMACHINE_SITE}"
-
-echo " ... remember to update composite files ... "
-
+./promoteRepo.sh ${BUILD_ID} ${DL_LABEL} ${DROP_TYPE}
