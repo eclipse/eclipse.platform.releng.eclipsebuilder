aca587bdb9e768b31e4480b60b956a5821cbecdd
David Williams <david_williams@us.ibm.com>
2012-07-06 00:40:42 -0400

    use java instead of javaw on windows (as on other platforms)

--- eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
@@ -8,6 +8,6 @@ REM set PATH=%PATH%;%executionDir\..\windowsBin
 REM run all tests
 REM runtests.bat -os win32 -ws win32 -arch x86_64 -vm c:\\java\\jdk1.6.0_20\\jre\\bin\\javaw -properties vm.properties > error.log
 mkdir results\consolelogs
-runtests.bat -os win32 -ws win32 -arch x86 -vm c:\\java\\jdk7u2\\jre\\bin\\javaw -properties vm.properties > results\consolelogs\win7consolelog.txt
+runtests.bat -os win32 -ws win32 -arch x86 -vm c:\\java\\jdk7u2\\jre\\bin\\java -properties vm.properties > results\consolelogs\win7consolelog.txt
 
 exit
