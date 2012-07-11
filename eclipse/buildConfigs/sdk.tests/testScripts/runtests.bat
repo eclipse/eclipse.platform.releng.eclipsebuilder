dce1235de95a9086c7a633bde0d6a559f291299b
David Williams <david_williams@us.ibm.com>
2012-07-11 12:50:36 -0400

    372880 trying to pass JAVA_ARGS explicitly

--- eclipse/buildConfigs/sdk.tests/testScripts/runtests.bat
+++ eclipse/buildConfigs/sdk.tests/testScripts/runtests.bat
@@ -67,7 +67,8 @@ rem -Dtimeout=1800000
 echo "list all environment variables in effect as tests start"
 set
 
-%vmcmd% -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch% -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
+# passing in JAVA_ARGS explicitly
+%vmcmd% %JAVA_ARGS% -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch% -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
 
 goto end
 
