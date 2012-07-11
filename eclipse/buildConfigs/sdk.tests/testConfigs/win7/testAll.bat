08730f8e039a6d2b7eca7cf707bf8d11102995dd
David Williams <david_williams@us.ibm.com>
2012-07-11 01:09:34 -0400

    fix local properties override filename

--- eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
@@ -9,7 +9,7 @@ REM but allows a place for people to have their own machines variables defined
 REM there so they do not have to hand edit each time to do a local build. 
 REM a typical example is that their version/location/vendor of VM is likely to differ,
 REM so they could redefine vmcmdvalue to what's appropriate to their machine and setup.
-call localTestsProperties.bat
+call localTestProperties.bat
 
 mkdir results\consolelogs
 runtests.bat -os win32 -ws win32 -arch x86 -vm %vmcmdvalue% -properties vm.properties > results\consolelogs\win7consolelog.txt
