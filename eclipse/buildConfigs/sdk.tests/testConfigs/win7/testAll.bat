45b566e57361fcbfdd4785e1afd71258a01ad3d2
David Williams <david_williams@us.ibm.com>
2012-07-11 01:03:38 -0400

    clean up comments

--- eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
@@ -2,9 +2,6 @@
 cd %executionDir
 REM test script
 
-REM add the extra binaries to the system path
-REM set PATH=%PATH%;%executionDir\..\windowsBin
-
 set vmcmdvalue=c:\\java\\jdk7u2\\jre\\bin\\java
 
 REM localTestProperties.bat is not used or expected in production environment, 
@@ -14,8 +11,6 @@ REM a typical example is that their version/location/vendor of VM is likely to d
 REM so they could redefine vmcmdvalue to what's appropriate to their machine and setup.
 call localTestsProperties.bat
 
-REM run all tests
-REM runtests.bat -os win32 -ws win32 -arch x86_64 -vm %vmcmdvalue% -properties vm.properties > error.log
 mkdir results\consolelogs
 runtests.bat -os win32 -ws win32 -arch x86 -vm %vmcmdvalue% -properties vm.properties > results\consolelogs\win7consolelog.txt
 
