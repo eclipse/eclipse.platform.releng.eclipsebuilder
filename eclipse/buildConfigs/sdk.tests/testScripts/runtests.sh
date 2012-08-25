5a90549c674581775fc0862a02e3d138dd5f1a17
David Williams <david_williams@us.ibm.com>
2012-08-24 21:03:12 -0400

    small change to "query system" calls

--- eclipse/buildConfigs/sdk.tests/testScripts/runtests.sh
+++ eclipse/buildConfigs/sdk.tests/testScripts/runtests.sh
@@ -103,33 +103,44 @@ then
     echo "list all environment variables in effect as tests start"
     printenv
     
-    echo "uname -a information"
+    echo "uname -a"
     uname -a
     echo 
-    
+    echo "lsb_release -a"
+    lsb_release -a
+    echo 
+        
 echo "cat /etc/lsb-release"
 cat /etc/lsb-release
+    echo 
 
 echo "cat /etc/SuSE-release"
 cat /etc/SuSE-release
+    echo 
 
 echo "rpm -q cairo"
 rpm -q cairo
+    echo 
 
 echo "rpm -q gtk2"
 rpm -q gtk2
+    echo 
 
 echo "rpm -q glibc"
 rpm -q glibc
+    echo 
 
 echo "rpm -q glib"
 rpm -q glib
+    echo 
 
 echo "rpm -q pango"
 rpm -q pango
+    echo 
 
 echo "rpm -q ORBit2"
 rpm -q ORBit2
+    echo 
 
 echo
     
