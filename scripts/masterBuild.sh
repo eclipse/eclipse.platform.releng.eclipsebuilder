2eef569c29b603ad30b11e63477d09dd1cbab117
David Williams <david_williams@us.ibm.com>
2012-09-17 22:25:41 -0400

    improve ability to do "local builds"

--- scripts/masterBuild.sh
+++ scripts/masterBuild.sh
@@ -142,7 +142,8 @@ updateBaseBuilder () {
         #cmd="cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse ${quietCVS} ex -r ${basebuilderBranch} -d org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder"
         # cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse ${quietCVS} ex -r ${basebuilderBranch} -d org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder
         # TODO: make cvs user/protocol/host variables so can be rrun remotely also
-        cvs -d :local:/cvsroot/eclipse ${quietCVS} ex -r ${basebuilderBranch} -d org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder
+        CVSROOT=${CVSROOT:-:local:/cvsroot/eclipse}
+        cvs -d ${CVSROOT} ${quietCVS} ex -r ${basebuilderBranch} -d org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder
         exitcode=$?
         #echo "cvs export cmd: ${cmd}"
         #"${cmd}"
