774ace926bb5c30e61f765d8883d625dca8c1381
David Williams <david_williams@us.ibm.com>
2012-09-18 20:18:02 -0400

    Bug 374441 - run performance tests on eclipse.org hardware

--- eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
@@ -2,6 +2,12 @@
 cd %executionDir
 REM test script
 
+if exists localTestsProperties.bat call localTestsProperties.bat
+
+# vm.properties is used by default on production machines, but will 
+# need to override on local setups and performance tests
+IF NOT DEFINED propertyFile SET propertyFile=vm.properties
+
 set vmcmdvalue=c:\\java\\jdk7u2\\jre\\bin\\javaw
 
 REM localTestProperties.bat is not used or expected in production environment, 
@@ -9,9 +15,9 @@ REM but allows a place for people to have their own machines variables defined
 REM there so they do not have to hand edit each time to do a local build. 
 REM a typical example is that their version/location/vendor of VM is likely to differ,
 REM so they could redefine vmcmdvalue to what's appropriate to their machine and setup.
-call localTestProperties.bat 2>nul
+if exists localTestProperties.bat call localTestProperties.bat 
 
 mkdir results\consolelogs
-runtests.bat -os win32 -ws win32 -arch x86 -vm %vmcmdvalue% -properties vm.properties %* > results\consolelogs\win7consolelog.txt
+runtests.bat -os win32 -ws win32 -arch x86 -vm %vmcmdvalue% -properties %propertyFile% %* > results\consolelogs\win7consolelog.txt
 
-exit
+exit 
