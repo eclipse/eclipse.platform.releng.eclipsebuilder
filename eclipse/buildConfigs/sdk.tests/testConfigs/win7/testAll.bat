11199f2738b5ce3c3f88fabe86bbe59e7e56258d
David Williams <david_williams@us.ibm.com>
2012-09-23 18:24:41 -0400

    Bug 374441 - run performance tests on eclipse.org hardware

--- eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
+++ eclipse/buildConfigs/sdk.tests/testConfigs/win7/testAll.bat
@@ -21,8 +21,11 @@ REM vm.properties is used by default on production machines, but will
 REM need to override on local setups and performance tests
 IF NOT DEFINED propertyFile SET propertyFile=vm.properties
 
+ECHO propertyFile: %propertyFile%
+
 IF NOT DEFINED vmcmd SET vmcmd=c:\\java\\jdk7u2\\jre\\bin\\javaw
 
+ECHO vmcmd: %vmcmd%
 
 mkdir results\consolelogs
 runtests.bat -os win32 -ws win32 -arch x86 -vm %vmcmd% -properties %propertyFile% %* > results\consolelogs\win7consolelog.txt
