46be7d891a39e6c075e7c03346166a3f761d0bdc
David Williams <david_williams@us.ibm.com>
2012-09-25 13:25:06 -0400

    prep to promote Juno SR1 (corrected to M build)

--- scripts/sdk/promotionRelatedRelease/promoteDropSite42.sh
+++ scripts/sdk/promotionRelatedRelease/promoteDropSite42.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # BUILD_ID is original I (or M) build on build machine that is being promoted
-BUILD_ID=I20120920-1300
+BUILD_ID=M20120914-1800
 # DL_LABEL takes form of 4.2M7, 4.2RC4, 4.2, etc.
 DL_LABEL=4.2.1
 # DROP_TYPE either S, R
