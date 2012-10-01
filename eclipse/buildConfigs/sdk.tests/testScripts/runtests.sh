c5d71304de91df2063645d2cd9c5eaa4b8398e1e
David Williams <david_williams@us.ibm.com>
2012-09-30 21:27:58 -0400

    Bug 390494 - performance tests fail during "transform" using new JRE

--- eclipse/buildConfigs/sdk.tests/testScripts/runtests.sh
+++ eclipse/buildConfigs/sdk.tests/testScripts/runtests.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "USER: $USER"
-echo "PATH: $PATH"
 # This file should never exist or be needed for production machine, 
 # but allows an easy way for a "local user" to provide this file 
 # somewhere on the search path ($HOME/bin is common), 
--- eclipse/buildConfigs/sdk.tests/testScripts/runtestsmac.sh
+++ eclipse/buildConfigs/sdk.tests/testScripts/runtestsmac.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# This file should never exist or be needed for production machine, 
+# but allows an easy way for a "local user" to provide this file 
+# somewhere on the search path ($HOME/bin is common), 
+# and it will be included here, thus can provide "override values" 
+# to those defined by defaults for production machine., 
+# such as for vmcmd
+
+source localTestsProperties.shsource
+
 # by default, use the java executable on the path for outer and test jvm
 #vmcmd=/shared/common/jdk-1.6.x86_64/jre/bin/java
 vmcmd=java
@@ -21,6 +30,9 @@ installmode="clean"
 # name of a property file to pass to Ant
 properties=
 
+# ext dir customization. Be sure "blank", if not defined explicitly on command line
+extdirproperty=
+
 # message printed to console
 usage="usage: $0 -os <osType> -ws <windowingSystemType> -arch <architecture> [-noclean] [<test target>][-properties <path>]"
 
@@ -28,20 +40,32 @@ usage="usage: $0 -os <osType> -ws <windowingSystemType> -arch <architecture> [-n
 # proces command line arguments
 while [ $# -gt 0 ]
 do
-	case "$1" in
-		-dir) dir="$2"; shift;;
-		-os) os="$2"; shift;;
-		-ws) ws="$2"; shift;;
-		-arch) arch="$2"; shift;;
-		-noclean) installmode="noclean";;
-		-properties) properties="-propertyfile $2";shift;;
-		-vm) vmcmd="$2"; shift;;
-		*) tests=$tests\ $1;;
+    case "${1}" in
+        -dir) 
+            dir="${2}"; shift;;
+        -os) 
+            os="${2}"; shift;;
+        -ws) 
+            ws="${2}"; shift;;
+        -arch) 
+            arch="${2}"; shift;;
+        -noclean) 
+            installmode="noclean";;
+        -properties) 
+            properties="-propertyfile ${2}";shift;;
+        -extdirprop) 
+            extdirproperty="-Djava.ext.dirs=${2}";shift;;    
+        -vm) 
+            vmcmd="${2}"; shift;;
+        *) 
+            tests=$tests\ ${1};;
 	esac
 	shift
 done
 
 echo "Specified test targets (if any): ${tests}"
+
+echo "Specified ext dir (if any): ${extdirproperty}"
 
 # for *nix systems, os, ws and arch values must be specified
 if [ "x$os" = "x" ]
@@ -71,20 +95,30 @@ then
 	if [ ! -r eclipse ]
 	then
 		tar -xzf eclipse-SDK-*.tar.gz
+        # note, the file pattern to match, must not start with */plugins because there is no leading '/' in the zip file, since they are repos.
 		unzip -qq -o -C eclipse-junit-tests-*.zip plugins/org.eclipse.test* -d eclipse/dropins/
 fi
 
 # run tests
 launcher=`ls eclipse/plugins/org.eclipse.equinox.launcher_*.jar`
 
-    echo "list all environment variables in effect as tests start"
-    printenv
+# it have been recommended not to "probe and publish" information about systems 
+# for slight improvement in security. Bug 387747
+# so I have commented out most such probes, so they won't be routine. 
+
+    #echo "list all environment variables in effect as tests start"
+    #printenv
 
 # -Dtimeout=300000 "${ANT_OPTS}"
-$vmcmd  -Dosgi.os=$os -Dosgi.ws=$ws -Dosgi.arch=$arch -jar $launcher -data workspace -application org.eclipse.ant.core.antRunner -file `pwd`/test.xml $tests -Dws=$ws -Dos=$os -Darch=$arch  -D$installmode=true $properties -logger org.apache.tools.ant.DefaultLogger
+ if [[ ! -z "${extdirproperty}" ]]
+ then 
+	$vmcmd "${extdirproperty}" -Dosgi.os=$os -Dosgi.ws=$ws -Dosgi.arch=$arch -XshowSettings  -jar $launcher -data workspace -application org.eclipse.ant.core.antRunner -file ${PWD}/test.xml $tests -Dws=$ws -Dos=$os -Darch=$arch -D$installmode=true $properties -logger org.apache.tools.ant.DefaultLogger
+ else
+	$vmcmd -Dosgi.os=$os -Dosgi.ws=$ws -Dosgi.arch=$arch -XshowSettings  -jar $launcher -data workspace -application org.eclipse.ant.core.antRunner -file ${PWD}/test.xml $tests -Dws=$ws -Dos=$os -Darch=$arch -D$installmode=true $properties -logger org.apache.tools.ant.DefaultLogger
+ fi		
 
-# display message to user if os, ws and arch are invalid
 else
+    # display message to user if os, ws and arch are invalid
 	echo "The os, ws and arch values are either invalid or are an invalid combination"
 
 exit 1
