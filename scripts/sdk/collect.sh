849dece97c1e57323e901b4a3314cd0ad8a39f1f
David Williams <david_williams@us.ibm.com>
2012-10-22 17:46:54 -0400

    Bug 389048 - test results sometimes do not get routed back to right download page summary (initial, partial work)

--- scripts/sdk/collect.sh
+++ scripts/sdk/collect.sh
@@ -1,10 +1,33 @@
+#!/usr/bin/env bash 
+
+# Utility to be called from test data collection cron job, 
+# to invoke the main code. expected to be called with piped 
+# input, such as
+# ./collect.sh < testjobs/testjobdata201210220811.txt
+
 
 export JAVA_HOME=/shared/common/jdk1.7.0
 export ANT_HOME=/shared/common/apache-ant-1.8.4
 
-${ANT_HOME}/bin/ant -f collectTestResults.xml \
-   -lib /shared/common/apache-ant-1.8.4/lib/ \
-   -Djob=ep3-unit-mac64 \
-   -DbuildNumber=5 \
-   -DbuildId=M20121017-1000  \
-   -DeclipseStream=3.8.2
+read inputline
+echo "inputline: $inputline"
+
+job="$(echo $inputline | cut -d\  -f1)"
+buildNumber="$(echo $inputline | cut -d\  -f2)"
+buildId="$(echo $inputline | cut -d\  -f3)"
+eclipseStream="$(echo $inputline | cut -d\  -f4)"
+
+echo "job: $job"
+echo "buildNumber: $buildNumber"
+echo "buildId: $buildId"
+echo "eclipseStream: $eclipseStream"
+
+# Uncomment once ready, but synchronize "release" of code with 
+# removing the check/wait loop in invokeTestsJSON.xml
+
+#${ANT_HOME}/bin/ant -f /shared/eclipse/sdk/collectTestResults.xml \
+   #   -lib /shared/common/apache-ant-1.8.4/lib/ \
+   #-Djob=${job} \
+   #-DbuildNumber=${buildNumber} \
+   #-DbuildId=${buildId} \
+   #-DeclipseStream=${eclipseStream}
