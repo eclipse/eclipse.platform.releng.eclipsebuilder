dec24fd558583ab75e32168e84646ea262a7c281
David Williams <david_williams@us.ibm.com>
2012-11-02 18:58:24 -0400

    Bug 390370 - Change to use release name instead of number on Equinox downloads page (Plus some overall improvements to automate)

--- scripts/sdk/renameBuild.sh
+++ scripts/sdk/renameBuild.sh
@@ -58,7 +58,7 @@ perl -w -pi -e ${replaceCommand} ${oldname}/checksum/*
 oldBuildName="Integration Build"
 newBuildName="Stable Build"
 replaceBuildNameCommand="s!${oldBuildName}!${newBuildName}!g"
-perl -w -pi -e ${replaceBuildNameCommand} ${newBuildName}/*.php
+perl -w -pi -e ${replaceBuildNameCommand} ${oldname}/*.php
 
 # move directory before file renames, so it won't be in file path name twice
 mv $oldname $newdirname
