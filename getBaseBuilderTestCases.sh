248b80a0d6c4df9014bc50d86ca166e0cd9df806
David Williams <david_williams@us.ibm.com>
2012-12-07 14:51:38 -0500

    Bug 395777 - Investigate converting basebuilder to Git

--- getBaseBuilderTestCases.sh
+++ getBaseBuilderTestCases.sh
@@ -19,3 +19,22 @@ mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-D-cvs
 ant -f getBaseBuilder.xml -DbuilderVersion=R38M6PlusRC3C -DfetchSource=cvs 
 mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-C-cvs 
 
+# try some older ones
+ant -f getBaseBuilder.xml -DbuilderVersion=R36_RC4
+mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-R36_RC4-git 
+
+ant -f getBaseBuilder.xml -DbuilderVersion=R36_RC4 -DfetchSource=cvs 
+mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-R36_RC4-cvs 
+
+ant -f getBaseBuilder.xml -DbuilderVersion=R37_M7
+mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-R37_M7-git 
+
+ant -f getBaseBuilder.xml -DbuilderVersion=R37_M7 -DfetchSource=cvs 
+mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-R37_M7-cvs 
+
+ant -f getBaseBuilder.xml -DbuilderVersion=R35_RC4
+mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-R35_RC4-git 
+
+ant -f getBaseBuilder.xml -DbuilderVersion=R35_RC4 -DfetchSource=cvs 
+mv org.eclipse.releng.basebuilder org.eclipse.releng.basebuilder-R35_RC4-cvs 
+
