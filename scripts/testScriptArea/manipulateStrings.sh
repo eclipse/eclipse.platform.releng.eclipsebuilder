e622ad247142733539bc285ea544926a775d2ccc
David Williams <david_williams@us.ibm.com>
2013-01-10 13:39:26 -0500

    minor changes to (pure) test scripts [not used in builds or tests]

--- scripts/testScriptArea/manipulateStrings.sh
+++ scripts/testScriptArea/manipulateStrings.sh
@@ -4,3 +4,9 @@
     eclipseStreamMajor=${eclipseStream:0:1}
     echo "eclipseStream: $eclipseStream"
     echo "eclipseStreamMajor: $eclipseStreamMajor"
+
+    
+    es=4.2.2
+esplain=${es//./}
+echo "es: $es"
+echo "esplain: $esplain"
