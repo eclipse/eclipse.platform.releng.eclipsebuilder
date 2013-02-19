1a913aefc5d6b0b0e1a91d97ab9e1e3b5532516d
David Williams <david_williams@us.ibm.com>
2013-02-18 23:38:15 -0500

    [releng] sync directory with that's on build machine

--- scripts/sdk/promotionRelatedRelease/promoteRepo38.sh
+++ scripts/sdk/promotionRelatedRelease/promoteRepo38.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # BUILD_ID is original I (or M) build on build machine that is being promoted
-BUILD_ID=M20120914-1540
+BUILD_ID=M20130131-0800
 # DL_LABEL takes form of 3.8M7, 3.8RC4, 3.8, etc.
 DL_LABEL=3.8.2
 # DROP_TYPE either R for Releases
--- scripts/sdk/promotionRelatedRelease/promoteRepo42.sh
+++ scripts/sdk/promotionRelatedRelease/promoteRepo42.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # BUILD_ID is original I (or M) build on build machine that is being promoted
-BUILD_ID=M20120914-1800
+BUILD_ID=M20130204-1200
 # DL_LABEL takes form of 3.8M7, 3.8RC4, 3.8, etc.
 DL_LABEL=4.2.2
 # DROP_TYPE either R for Releases
