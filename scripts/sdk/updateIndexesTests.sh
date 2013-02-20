389821d482b64928fbf603cf8b4dc7f1709530e4
David Williams <david_williams@us.ibm.com>
2013-02-19 21:10:15 -0500

    [releng] fix update Index pages function

--- scripts/sdk/updateIndexesTests.sh
+++ scripts/sdk/updateIndexesTests.sh
@@ -9,7 +9,24 @@ then
 else
     source updateIndexFilesFunction.shsource
 fi
+
+echo "= = = = invalid args"
+updateIndex xys
+updateIndex 8
+updateIndex 8 PDE
+updateIndex 3 CBIxx
+echo "= = = = dual args" 
 updateIndex 3 PDE
 updateIndex 4 PDE
 updateIndex 3 CBI
 updateIndex 4 CBI
+echo "= = = = single arg"
+unset MAJOR BUILD_TECH
+updateIndex 3
+updateIndex 4
+updateIndex PDE
+updateIndex CBI
+echo "= = = = no arg"
+updateIndex 
+
+
